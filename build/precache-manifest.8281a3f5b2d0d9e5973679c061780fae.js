a9d36cd5e1032e55ee799bdfd3986172551a225b
Javiconlinea <Javico@users.noreply.github.com>
2020-05-03 13:51:03 -0500

    Add files via upload

---
+++ build/precache-manifest.8281a3f5b2d0d9e5973679c061780fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162c3725d4545d64d967ac652a9bb091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfc7c3ca38cd6e537b2",
+    "url": "/static/css/main.92c30ab4.chunk.css"
+  },
+  {
+    "revision": "2079161da8d6b71610c1",
+    "url": "/static/js/2.b0975570.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b0975570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfc7c3ca38cd6e537b2",
+    "url": "/static/js/main.fc6988a3.chunk.js"
+  },
+  {
+    "revision": "f0627556ccb3129abc3f",
+    "url": "/static/js/runtime-main.7234a6a5.js"
+  }
+]);
